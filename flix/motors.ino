192d99e5e23aaebd73e6433ee5cad5944615c921
Arthur Golubtsov <goldartt@gmail.com>
2026-06-27 00:50:23 +0300

    Change default pwmFrequency to 38023 for esp32-s3

--- flix/motors.ino
+++ flix/motors.ino
@@ -8,7 +8,7 @@
 float motors[4]; // normalized motor thrusts in range [0..1]
 
 int motorPins[4] = {10, 11, 12, 13}; // default pin numbers
-int pwmFrequency = 40000;
+int pwmFrequency = 38023;
 int pwmResolution = 10;
 int pwmStop = 0;
 int pwmMin = 0;
