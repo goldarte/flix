97bff86c72c42455f5aa61f80343e30dd2285f5a
Arthur Golubtsov <goldartt@gmail.com>
2026-07-25 08:02:44 +0300

    Add definitions to fix arduino builds

--- flix/flix.ino
+++ flix/flix.ino
@@ -6,6 +6,9 @@
 #include "vector.h"
 #include "quaternion.h"
 #include "util.h"
+#include <MAVLink.h>
+
+class PID;
 
 extern float t, dt;
 extern float controlRoll, controlPitch, controlYaw, controlThrottle, controlMode;
@@ -13,6 +16,10 @@ extern Vector gyro, acc;
 extern Vector rates;
 extern Quaternion attitude;
 extern Vector position, velocity;
+extern Quaternion odometryAttitude;
+extern float odometryTime;
+extern Vector positionTarget, velocityTarget, accelerationTarget;
+extern float positionYawTarget, positionYawRateTarget;
 extern bool positionControlActive;
 extern bool landed;
 extern float motors[4];
